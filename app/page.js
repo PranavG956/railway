7334f43134cb776e72b29433bef9e5bfacf15f33
PranavG956 <pranavgujjar2006@gmail.com>
2025-08-07 00:00:24 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,8 +8,8 @@ export default function Home() {
   const router = useRouter()
   return (
     <div className="min-h-[calc(100vh-88px)] bg-white flex flex-col justify-center items-center">
-      <h1 className="text-4xl text-center font-bold mb-4">Metro Booking System</h1>
-      <p className="text-lg text-center text-gray-600 mb-8">Book your metro tickets easily and quickly online.</p>
+      <h1 className="text-4xl text-center font-bold mb-4">Railway Booking System</h1>
+      <p className="text-lg text-center text-gray-600 mb-8">Book your railway tickets easily and quickly online.</p>
       <div className="flex gap-4">
         <button onClick={()=>{router.push("/about")}} className="cursor-pointer px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">More</button>
         <button onClick={()=>{router.push(session ? "/dashboard" : "/login")}} className="cursor-pointer px-6 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition">Get Started</button>
